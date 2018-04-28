be096f33dace0025bea61f0e2e83453879f8da8a
STRYCHNINA <nataliawelter@wp.pl>
2018-04-28 22:54:32 +0200

    Add examples in WielkieLiczby.java

--- exercise java/WielkieLiczby.java
+++ exercise java/WielkieLiczby.java
@@ -43,7 +43,7 @@
 
       Do operacji matematycznych na wielkich liczbach nie możemy używac standardowych operatorów.
       Należy użyć gotowych funkcji typu: 
-      add(); substract(); multiply(); divide();
+      add(); subtract(); -UWAGA! nie 'subStract'! multiply(); divide();
 
       */
 
@@ -54,6 +54,52 @@
       //deklaracja zmiennej zmiennoprzecinkowej
       BigDecimal wielka_dec = BigDecimal.valueOf(123.459382);
       System.out.println(wielka_dec);
+
+      //operacje matematyczne - dodawanie a.add(b);
+
+      BigInteger suma = wielka.add(wielka2);
+      System.out.println("Suma "+suma);
+      //drugi poprawny zapis:
+      System.out.println("Suma "+suma.toString());
+
+      //Przykładowe zadania:
+
+      BigInteger wielkaLiczba = new BigInteger("938217621726189202812801021");
+      BigInteger wielkaLiczba2 = new BigInteger("09283873277287287126182192");
+      BigInteger ujemna = new BigInteger("-18271726812718");
+      int exponent = 3;
+
+      //suma:
+      BigInteger suma_liczb = wielkaLiczba.add(wielkaLiczba2);
+
+      //różnica
+      BigInteger roznica_liczb = wielkaLiczba.subtract(wielkaLiczba2);
+
+      //mnozenie
+
+      BigInteger iloczyn_liczb = wielkaLiczba.multiply(wielkaLiczba2);
+
+      //potega
+
+      BigInteger potega_3 = wielkaLiczba.pow(exponent);
+
+      //wartosc bezwzgledna -> sposób zapisu!
+
+      BigInteger absolute = wielkaLiczba2.abs();
+      BigInteger absolute2 = ujemna.abs();
+
+      System.out.println("Liczba 1: "+wielkaLiczba);
+      System.out.println("Liczba 2: "+wielkaLiczba2);
+      System.out.println("Liczba 3: "+ujemna);
+      System.out.println("Suma wynosi: "+suma_liczb+",\n roznica wynosi "+roznica_liczb+", \n iloczyn wynosi "+iloczyn_liczb);
+      System.out.println(wielkaLiczba+" do potegi 3 wynosi "+potega_3);
+      System.out.println("wartosc bezwzgledna liczby 2. wynosi: "+absolute);
+      System.out.println("wartosc bezwzgledna liczby 3. wynosi: "+absolute2);
+
+
+
+
+
 
 
 
