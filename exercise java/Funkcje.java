bd382007fa654daea19b9ab2b84a8a3852028ef5
STRYCHNINA <nataliawelter@wp.pl>
2018-04-28 22:25:59 +0200

    Add examples in Funkcje.java

--- exercise java/Funkcje.java
+++ exercise java/Funkcje.java
@@ -14,6 +14,33 @@ public class Funkcje{
     System.out.println("Liczba "+liczba+" podniesiona do potegi "+b+" wynosi: "+potega);
     System.out.println("Pi wynosi "+pi);
 
+    //zadanie-prosty kalkulator:
+
+    int c = -5;
+    int d = 6;
+
+    potega = pow(c,d);
+    int bezwzgledna = abs(c);
+    pierwiastek = sqrt(pow(c,d));
+
+    System.out.println("Liczba "+c+" podniesiona do potegi "+d+" wynosi "+potega);
+    System.out.println("Wartosc bezwzgledna liczby "+c+" wynosi "+bezwzgledna);
+    System.out.println("Pierwiastek kwadratowy z -5 podniesionej do potegi 6 wynosi "+pierwiastek);
+
+    //operacje na 3-cyfrowych zmiennych int:
+
+    c = 324;
+    d = 111;
+
+    potega = pow(c,d);
+    bezwzgledna = abs(c);
+    pierwiastek = sqrt(pow(c,d));
+
+    System.out.println("Liczba "+c+" podniesiona do potegi "+d+" wynosi "+potega);
+    System.out.println("Wartosc bezwzgledna liczby "+c+" wynosi "+bezwzgledna);
+    System.out.println("Pierwiastek kwadratowy z 324 podniesionej do potegi 111 wynosi "+pierwiastek);
+
+    //wyniki sa wyświetlane w zapisie naukowym
 
   }
 }
