b7f706868983d0f8b39065c061e9dd7e5cdab88d
STRYCHNINA <nataliawelter@wp.pl>
2018-05-07 22:59:27 +0200

    Add examples of methods in AccessModifiers.java

--- exercise java/Chapter07Udemy/src/oop/MainClass.java
+++ exercise java/Chapter07Udemy/src/oop/MainClass.java
@@ -32,6 +32,24 @@ public class MainClass {
 		
 		AccessModifiers test = new AccessModifiers (10, 12, 14);
 		
+		//first method calling:
+		
+		testing.displayHello();
+		
+		//second method calling:
+		
+		System.out.println("The sum is: "+testing.addTwoNumbers(3, 65.56));
+		
+		//third method calling:
+		
+		testing.greaterNumb(21, 43);
+		testing.greaterNumb(11, 11);
+		testing.greaterNumb(-2, -113);
+		
+		//fourth method calling:
+		
+		testing.printOut("Something will be printed out!");
+		
 		
 	}
 
